20ab66a12289ef62c13f696fb30851b34abe5b55
Ahmad Jerjees <ajerjees010@gmail.com>
2025-06-23 06:08:37 +0300

    Add project files.

---
+++ KoperasiRegistrationAPI/Data/KoperasiAccountsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using KoperasiRegistrationAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KoperasiRegistrationAPI.Data;
+
+public class KoperasiAccountsDbContext : DbContext
+{
+    public KoperasiAccountsDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    public DbSet<Account> Accounts { get; set; }
+}
+    
+        
+
---
+++ KoperasiRegistrationAPI/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace KoperasiRegistrationAPI.Models;
+
+public class Account
+{
+    public int Id { get; set; }
+    public string? CustomerName { get; set; }
+    public string? ICNumber { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? Email { get; set; }
+}
